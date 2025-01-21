37850a0d4dc9bf63e074da496c021ef88c4d5d92
Whyzow <wizoremainrb@gmail.com>
2025-01-21 07:13:19 +0000

    Submit

---
+++ next_year.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+name= input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+#stuff you 
+print(f"Hello, {name}! You are {age} years old.")
+print(f"Next year you will be {age + 1} years old.")
